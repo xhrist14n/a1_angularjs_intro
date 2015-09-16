c1ea4e62b521ead0abe564bb36312c7737a1cb3a
Developer <khrist14n@gmail.com>
2015-09-16 13:03:08 -0500

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name adminApp
+ * @description
+ * # adminApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('adminApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    
+  }
+);
